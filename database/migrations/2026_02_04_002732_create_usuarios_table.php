8406274d6dd58893ae6e84aa312e0c80b1793867
Jonathan M <marinjonathan40@gmail.com>
2026-02-04 00:52:30 -0500

    Creación proyecto - Migraciones
    se crea el proyecto y las migraciones

---
+++ database/migrations/2026_02_04_002732_create_usuarios_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',45);
+            $table->string('correo')->unique();
+            $table->string('contrasena');
+            $table->rememberToken();
+            $table->timestamps();
+            
+
+            $table->foreignId('rol_id')->constrained('rols')->restrictOnDelete();
+
+
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('correo')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('usuario_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
