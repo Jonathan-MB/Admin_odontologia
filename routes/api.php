3f54e2ec4a30c674749326409c9843a47dfd0bbd
Jonathan M <marinjonathan40@gmail.com>
2026-02-11 13:12:40 -0500

    Crear rutas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,37 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\DienteController;
+use App\Http\Controllers\EpsController;
+use App\Http\Controllers\EspecialistaController;
+use App\Http\Controllers\FacturaController;
+use App\Http\Controllers\GrupoController;
+use App\Http\Controllers\HistoriaController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\SedeController;
+use App\Http\Controllers\TipoDocumentoController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/test', function () {
+    return 'hola';
+});
+
+Route::group(['prefix'=>'v1', 'namespace' => 'App\Http\Controllers'], function(){
+    Route::apiResource('dientes',DienteController::class);
+    Route::apiResource('clientes',ClienteController::class);
+    Route::apiResource('especialistas',EspecialistaController::class);
+    Route::apiResource('facturas',FacturaController::class);
+    Route::apiResource('grupos',GrupoController::class);
+    Route::apiResource('historias',HistoriaController::class);
+    Route::apiResource('rols',RolController::class);
+    Route::apiResource('sedes',SedeController::class);
+    Route::apiResource('tipoDocumentos',TipoDocumentoController::class);
+    Route::apiResource('usuarios',UsuarioController::class);
+    Route::apiResource('eps', EpsController::class)->parameters(['eps' => 'eps']);
+    Route::post('historias/bulk',['uses'=>'HistoriaController@bulkStore']);
+});
