46b55b7feb7f0d783811dd7d1f46b672b0198e9b
Jonathan M <marinjonathan40@gmail.com>
2026-02-11 13:09:00 -0500

    Se crearon Resource / Collection

---
+++ app/Http/Resources/FacturaResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FacturaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'clienteId'         => $this->cliente_id,
+            'especialistaId'    => $this->especialista_id,
+            'fecha'             => Carbon::parse($this->created_at)
+                ->setTimezone('America/Bogota')
+                ->format('H:i:s d-m-Y'),
+            'abono'             => $this->abono,
+            'saldo'             => $this->saldo,
+        ];
+    }
+}
